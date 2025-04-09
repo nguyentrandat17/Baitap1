f42f3b853466c3854d8f33cf1aefb2f3ffbd2442
nguyentrandat17 <160696219+nguyentrandat17@users.noreply.github.com>
2025-04-09 19:36:29 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,96 +1,84 @@
-﻿// Bài 1: Nhập tên và tuổi, sau đó in ra màn hình thông báo
-using System;
-
-namespace BasicExercises
+﻿//Bài 1: Viết một hàm để tính tổng của tất cả các số chẵn trong một mảng.
+int TinhTongChan(int[] arr)
 {
-    class Program
+    int tong = 0;
+    foreach (int x in arr)
     {
-        static void Main(string[] args)
+        if (x % 2 == 0)
+            tong += x;
+    }
+    return tong;
+}
+
+//Bài 2: Viết chương trình nhập vào mảng gồm n phần tử nhập từ bàn phím. Viết hàm để kiểm
+//tra xem một số có phải là số nguyên tố hay không, hiển thị chỉ số và giá trị của những phần tử
+//là số nguyên tố trong mảng.
+bool LaSoNguyenTo(int n)
+{
+    if (n < 2) return false;
+    for (int i = 2; i <= Math.Sqrt(n); i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void InSoNguyenTo(int[] arr)
+{
+    Console.WriteLine("Các số nguyên tố trong mảng:");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (LaSoNguyenTo(arr[i]))
+            Console.WriteLine($"Chỉ số: {i}, Giá trị: {arr[i]}");
+    }
+}
+
+//Bài 3: Viết một hàm để đếm số lượng số âm và số dương trong một mảng gồm n phần tử nhập
+//từ bàn phím.
+void DemSoAmDuong(int[] arr, out int soAm, out int soDuong)
+{
+    soAm = 0;
+    soDuong = 0;
+    foreach (int x in arr)
+    {
+        if (x < 0) soAm++;
+        else if (x > 0) soDuong++;
+    }
+}
+
+//Bài 4: Viết hàm để tìm số lớn thứ hai trong một mảng các số nguyên.
+int TimSoLonThuHai(int[] arr)
+{
+    int max = int.MinValue;
+    int secondMax = int.MinValue;
+
+    foreach (int x in arr)
+    {
+        if (x > max)
         {
-        Console.Write("Nhập tên của bạn: ");
-        string name = Console.ReadLine();
-        Console.Write("Nhập tuổi của bạn: ");
-        string age = Console.ReadLine();
-        Console.WriteLine($"Xin chào {name}, bạn {age} tuổi!");
-
-  // Bài 2
-        Console.Write("Nhập chiều dài: ");
-        double length = Convert.ToDouble(Console.ReadLine());
-        Console.Write("Nhập chiều rộng: ");
-        double width = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine($"Diện tích hình chữ nhật: {length * width}");
-  // Bài 3
-            Console.Write("Nhập nhiệt độ (°C): ");
-            double celsius = Convert.ToDouble(Console.ReadLine());
-            double fahrenheit = (celsius * 9 / 5) + 32;
-            Console.WriteLine($"Nhiệt độ tương ứng: {fahrenheit}°F");
-
-  // Bài 4
-            Console.Write("Nhập một số nguyên: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            if (num % 2 == 0)
-                Console.WriteLine("Số chẵn");
-            else
-                Console.WriteLine("Số lẻ");
-
-  // Bài 5
-            Console.Write("Nhập số thứ nhất: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Nhập số thứ hai: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"Tổng: {num1 + num2}, Tích: {num1 * num2}");
-
-  // Bài 6
-            Console.Write("Nhập một số: ");
-            double number = Convert.ToDouble(Console.ReadLine());
-            if (number > 0)
-                Console.WriteLine("Số dương");
-            else if (number < 0)
-                Console.WriteLine("Số âm");
-            else
-                Console.WriteLine("Số không");
-
-   // Bài 7
-            Console.Write("Nhập năm: ");
-            int year = Convert.ToInt32(Console.ReadLine());
-            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-                Console.WriteLine("Năm nhuận");
-            else
-                Console.WriteLine("Không phải năm nhuận");
-
-   // Bài 8
-            for (int i = 1; i <= 10; i++)
-            {
-                for (int j = 1; j <= 10; j++)
-                {
-                    Console.Write($"{i} x {j} = {i * j}\t");
-                }
-                Console.WriteLine();
-            }
-
-    // Bài 9
-            Console.Write("Nhập số nguyên dương: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            long factorial = 1;
-            for (int i = 1; i <= n; i++)
-            {
-                factorial *= i;
-            }
-            Console.WriteLine($"Giai thừa của {n} là {factorial}");
-
-    // Bài 10
-            Console.Write("Nhập một số nguyên: ");
-            int primeCheck = Convert.ToInt32(Console.ReadLine());
-            bool isPrime = primeCheck > 1;
-            for (int i = 2; i <= Math.Sqrt(primeCheck); i++)
-            {
-                if (primeCheck % i == 0)
-                {
-                    isPrime = false;
-                    break;
-                }
-            }
-            Console.WriteLine(isPrime ? "Là số nguyên tố" : "Không phải số nguyên tố");
+            secondMax = max;
+            max = x;
+        }
+        else if (x > secondMax && x < max)
+        {
+            secondMax = x;
         }
     }
+
+    return secondMax;
+}
+
+//Bài 5: Viết hàm hoán vị 2 số nguyên a và b nhập từ bàn phím.
+void HoanVi(ref int a, ref int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+//Bài 6: Viết hàm sắp xếp mảng số thực n phần tử nhập từ bàn phím theo chiều tăng dần.
+void SapXepTangDan(double[] arr)
+{
+    Array.Sort(arr); // Cách đơn giản nhất
 }
